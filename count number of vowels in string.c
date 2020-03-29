1e419d8c0ca9f3dc155421f16c36e50b52195551
shrutivohra <62501518+shrutivohra@users.noreply.github.com>
2020-03-29 22:53:33 +0530

    file updated

--- count number of vowels in string.c
+++ count number of vowels in string.c
@@ -3,8 +3,8 @@ int main()
 {
 char a[100];
 int i,count=0;
-scanf("%[^\n],a);
-for(i=0;a[i]!='\0;i++)
+scanf("%[^\n]",a);
+for(i=0;a[i]!='\0';i++)
 {
 if(a[i]=='a'||a[i]=='e'||a[i]=='o'||a[i]=='u'||a[i]=='i'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
 {
